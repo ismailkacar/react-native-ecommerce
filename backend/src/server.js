1420787d9c7aff2d09f585060b7ac7635352505a
İsmail Kaçar <isokacar68@gmail.com>
2026-06-24 23:00:02 +0300

    db config & tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from '@clerk/express'
+
+
+dotenv.config();
 
 const app = express();
 
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello, World!" });
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    connectDB();
 });
 
