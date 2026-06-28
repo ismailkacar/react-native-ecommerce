0e07be9f451e429683aa3aad7adf8b87d4b24447
İsmail Kaçar <isokacar68@gmail.com>
2026-06-28 14:02:24 +0300

    created mongodb models and integrate Inngest for user synchronization and deletion

---
+++ backend/src/config/inngest.js
@@ -0,0 +1,36 @@
+import inngest from "inngest";
+import { connectDB } from "./db.js";
+import { User } from "../models/user.model.js";
+
+export const syncUser = inngest.createFunction(
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+    const { id, email_addresses, first_name, last_name, image_url } =
+      event.data;
+
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}` || "User",
+      imageUrl: image_url,
+      adresses: [],
+      wishlist: [],
+    };
+
+    await User.create(newUser);
+  },
+);
+
+export const deleteUserFromDB = inngest.createFunction(
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+  },
+);
+
+export const functions = [syncUser, deleteUserFromDB];
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import { clerkMiddleware } from '@clerk/express'
-
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./config/inngest.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
+app.use(serve(functions));
 app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
-    res.json({ message: "Hello, World!" });
+  res.json({ message: "Hello, World!" });
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-    connectDB();
-});
+app.use("api/inngest", serve({ client: inngest, functions: functions }));
 
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error(`Error starting server: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+startServer();
